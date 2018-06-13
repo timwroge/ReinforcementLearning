0cd54eee2ddbaa9edb887eaa2a550e4030be1ae4
Tim Wroge <tim@COELHO.UBUNTU>
2018-06-13 11:40:26 +0700

    Added simple q learning scripts for agents

---
+++ src/QLearning/taxiExample.py
@@ -0,0 +1,45 @@
+import gym
+env = gym.make("Taxi-v2")
+
+n_actions = env.action_space.n
+from qlearning import QLearningAgent
+
+agent = QLearningAgent(alpha=0.5, epsilon=0.25, discount=0.99,
+                       get_legal_actions = lambda s: range(n_actions))
+def play_and_train(env,agent,t_max=10**4):
+    """
+    This function should
+    - run a full game, actions given by agent's e-greedy policy
+    - train agent using agent.update(...) whenever it is possible
+    - return total reward
+    """
+    total_reward = 0.0
+    s = env.reset()
+
+    for t in range(t_max):
+        # get agent to pick action given state s.
+        a = <YOUR CODE>
+
+        next_s, r, done, _ = env.step(a)
+
+        # train (update) agent for state s
+        <YOUR CODE HERE>
+
+        s = next_s
+        total_reward +=r
+        if done: break
+
+    return total_reward
+
+
+
+rewards = []
+for i in range(1000):
+    rewards.append(play_and_train(env, agent))
+    agent.epsilon *= 0.99
+
+    if i %100 ==0:
+        clear_output(True)
+        print('eps =', agent.epsilon, 'mean reward =', np.mean(rewards[-10:]))
+        plt.plot(rewards)
+        plt.show()
