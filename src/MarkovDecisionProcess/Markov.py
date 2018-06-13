1dcfc29d0c5d6b954b100e31f695c37b786f9dc1
Tim Wroge <tim@COELHO.UBUNTU>
2018-06-13 11:26:35 +0700

    Modified save file and added folder for markov decision processes

---
+++ src/MarkovDecisionProcess/Markov.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+import numpy
+import gym
+
+#make gym environment
+
+#create policy matrix
+
+#optimize with cross entropy
+
+#run
+if __name__ == '__main__' :
+    run()
