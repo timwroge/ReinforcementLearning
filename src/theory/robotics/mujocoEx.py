ee26a6e186bb6716cde49e3c9213048efda78d06
Tim Wroge <tim@COELHO.UBUNTU>
2018-06-20 22:23:21 +0700

    Added simple mujoco model

---
+++ src/theory/robotics/mujocoEx.py
@@ -0,0 +1,177 @@
+import numpy as np
+import sys
+sys.path.insert(0, '../QLearning/' )
+from simpleQLearning import QLearningAgent
+import matplotlib.pyplot as plt
+import os
+import random
+from collections import deque
+class ReplayBuffer(object):
+    def __init__(self, size):
+        """
+        Create Replay buffer.
+        Parameters
+        ----------
+        size: int
+            Max number of transitions to store in the buffer. When the buffer
+            overflows the old memories are dropped.
+
+        Note: for this assignment you can pick any data structure you want.
+              If you want to keep it simple, you can store a list of tuples of (s, a, r, s') in self._storage
+              However you may find out there are faster and/or more memory-efficient ways to do so.
+        """
+        self._storage =deque([],size )
+        #initialize the storage as all nones
+        for _ in range( size):
+            self._storage.appendleft(None)
+        self._maxsize = size
+
+        # OPTIONAL: YOUR CODE
+
+
+    def __len__(self):
+        return len(self._storage)
+
+    def add(self, obs_t, action, reward, obs_tp1, done):
+        '''
+        Make sure, _storage will not exceed _maxsize.
+        Make sure, FIFO rule is being followed: the oldest examples has to be removed earlier
+        '''
+        data = (obs_t, action, reward, obs_tp1, done)
+
+        # add data to storage
+        self._storage.appendleft(data)
+    def sample(self, batch_size):
+        """Sample a batch of experiences.
+        Parameters
+        ----------
+        batch_size: int
+            How many transitions to sample.
+        Returns
+        -------
+        obs_batch: np.array
+            batch of observations
+        act_batch: np.array
+            batch of actions executed given obs_batch
+        rew_batch: np.array
+            rewards received as results of executing act_batch
+        next_obs_batch: np.array
+            next set of observations seen after executing act_batch
+        done_mask: np.array
+            done_mask[i] = 1 if executing act_batch[i] resulted in
+            the end of an episode and 0 otherwise.
+        """
+        idxes = np.random.permutation(self._maxsize ) #<randomly generate batch_size integers to be used as indexes of samples>
+
+        randBatch=[]
+        for index in idxes:
+            randBatch.append(self._storage[index] )
+
+        states=[]
+        actions=[]
+        rewards=[]
+        next_states=[]
+        is_done=[]
+
+        # collect <s,a,r,s',done> for each index
+        for tuple_vals in randBatch:
+            if tuple_vals != None:
+                states.append(tuple_vals[0])
+                actions.append(tuple_vals[1] )
+                rewards.append(tuple_vals[2] )
+                next_states.append(tuple_vals[3] )
+                is_done.append(tuple_vals[4] )
+
+
+        return np.array(states), np.array(actions), np.array(rewards), np.array(next_states), np.array(is_done)
+
+#testing to see if the buffer works correctly:
+replay = ReplayBuffer(2)
+obj1 = tuple(range(5))
+obj2 = tuple(range(5, 10))
+replay.add(*obj1)
+assert replay.sample(1)==obj1, "If there's just one object in buffer, it must be retrieved by buf.sample(1)"
+replay.add(*obj2)
+assert len(replay._storage)==2, "Please make sure __len__ methods works as intended."
+replay.add(*obj2)
+assert len(replay._storage)==2, "When buffer is at max capacity, replace objects instead of adding new ones."
+assert tuple(np.unique(a) for a in replay.sample(100))==obj2
+replay.add(*obj1)
+assert max(len(np.unique(a)) for a in replay.sample(100))==2
+replay.add(*obj1)
+assert tuple(np.unique(a) for a in replay.sample(100))==obj1
+print ("Success!")
+
+import gym
+
+env = gym.make("HandManipulateBlock-v0")
+n_actions = env.action_space.n
+
+def play_and_train_with_replay(env, agent, replay=None,
+                               t_max=10**4, replay_batch_size=32):
+    """
+    This function should
+    - run a full game, actions given by agent.getAction(s)
+    - train agent using agent.update(...) whenever possible
+    - return total reward
+    :param replay: ReplayBuffer where agent can store and sample (s,a,r,s',done) tuples.
+        If None, do not use experience replay
+    """
+    total_reward = 0.0
+    s = env.reset()
+
+    for t in range(t_max):
+        # get agent to pick action given state s
+        a=agent.get_action(s)
+        next_s, r, done, _ = env.step(a)
+        # update agent on current transition. Use agent.update
+
+        agent.update(s, a, r, next_s)
+
+        if replay is not None:
+            # store current <s,a,r,s'> transition in buffer
+
+            # sample replay_batch_size random transitions from replay,
+            # then update agent on each of them in a loop
+            #obs_t, action, reward, obs_tp1, done)
+            replay.add(s,a,r,next_s, done)
+        s = next_s
+        total_reward +=r
+        if done:break
+
+    return total_reward
+# Create two agents: first will use experience replay, second will not.
+
+agent_baseline = QLearningAgent(alpha=0.5, epsilon=0.25, discount=0.99,
+                       get_legal_actions = lambda s: range(n_actions))
+
+agent_replay = QLearningAgent(alpha=0.5, epsilon=0.25, discount=0.99,
+                       get_legal_actions = lambda s: range(n_actions))
+
+replay = ReplayBuffer(10000)
+
+import pandas
+from pandas import Series
+
+rewards_replay, rewards_baseline = [], []
+#record every  episode with replay
+env = gym.wrappers.Monitor(env, 'videos', video_callable=lambda episode_id: episode_id%400==0, force=True)
+
+
+for i in range(10000):
+    rewards_replay.append(play_and_train_with_replay(env, agent_replay, replay))
+    rewards_baseline.append(play_and_train_with_replay(env, agent_baseline, replay=None))
+
+
+    agent_replay.epsilon *= 0.99
+    agent_baseline.epsilon *= 0.99
+    env.render()
+    if i %100 ==0:
+        print('Baseline : eps =', agent_replay.epsilon, 'mean reward =', np.mean(rewards_baseline[-10:]))
+        print('ExpReplay: eps =', agent_baseline.epsilon, 'mean reward =', np.mean(rewards_replay[-10:]))
+        #plt.plot(moving_average(rewards_replay), label='exp. replay')
+        #plt.plot(moving_average(rewards_baseline), label='baseline')
+        plt.grid()
+        plt.legend()
+        #plt.show()
+
